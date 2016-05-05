f9cb3e8c1c91f738139633965305aa5b202210a8
AlanP <pwalan@qq.com>
2016-05-05 11:25:53 +0800

    some change

--- src/action/UserAction.java
+++ src/action/UserAction.java
@@ -86,12 +86,6 @@ public class UserAction implements Action {
 		this.userService = userService;
 	}
 	
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
 	@Override
 	public String execute() throws Exception {
 		// TODO Auto-generated method stub
